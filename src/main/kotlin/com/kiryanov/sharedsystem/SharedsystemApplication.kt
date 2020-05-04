b7dfe250ec0056f9993736ab07db7b1760b96042
Evgeniy Kiryanov <evgeniy.kiryanov@roonyx.tech>
2020-05-04 16:09:54 +0300

    initial commit

---
+++ src/main/kotlin/com/kiryanov/sharedsystem/SharedsystemApplication.kt
@@ -0,0 +1,11 @@
+package com.kiryanov.sharedsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SharedsystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<SharedsystemApplication>(*args)
+}
